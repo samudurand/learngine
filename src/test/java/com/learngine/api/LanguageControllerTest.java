7c9febcb22f87da65e440b0bfea4880b8fc65378
Samuel Durand <Samuel.Durand@digital.homeoffice.gov.uk>
2020-05-09 16:30:48 +0100

    Change translate with a mock to avoid credential mocking difficultise

--- src/test/java/com/learngine/api/LanguageControllerTest.java
+++ src/test/java/com/learngine/api/LanguageControllerTest.java
@@ -6,10 +6,12 @@ import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 import com.learngine.api.model.TextToTranslate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,26 +29,21 @@ import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static com.github.tomakehurst.wiremock.client.WireMock.reset;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-@ExtendWith(WireMockExtension.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureWebTestClient
 public class LanguageControllerTest {
-
-    static final int WIREMOCK_PORT = 4000;
-
-    @Managed
-    WireMockServer wireMockServer = with(wireMockConfig().port(WIREMOCK_PORT));
 
     @Autowired
     private WebTestClient client;
 
     @Test
     public void translateFromEnglishToItalian() throws Exception {
-        stubFor(get("/language/translate/v2?q=hello&source=en&target=it")
-                .willReturn(okJson("{\"data\": {\"translations\": [{\"translatedText\":\"Ciao\"}]}}")));
-
         client.post()
                 .uri("/languages/translate")
                 .body(BodyInserters.fromValue(new TextToTranslate("hello", "it")))
@@ -63,7 +60,11 @@ public class LanguageControllerTest {
         @Bean
         @Primary
         public Translate translate() {
-            return TranslateOptions.newBuilder().setHost("http://localhost:" + WIREMOCK_PORT).build().getService();
+            var mockService = mock(Translate.class);
+            var mockTranslation = mock(Translation.class);
+            when(mockTranslation.getTranslatedText()).thenReturn("Ciao");
+            when(mockService.translate(anyString(), any(), any())).thenReturn(mockTranslation);
+            return mockService;
         }
     }
 }
